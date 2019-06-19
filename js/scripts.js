d019b420790740b59ecfd68a805cd565f7f6fbe1
Christina Greene <christina.greene@rogers.com>
2019-06-19 13:27:50 -0400

    adjusted hamburger menu when open / tweaked responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
 $(document).ready(function () {
    //hamburger menu open/close
    $('.navIcon').click(function () {
+      // toggles class for hamburger menu turning into an X
       $(this).toggleClass('open');
       
-
-      const element = document.getElementById('menuId'),
-      style = window.getComputedStyle(element),
-      left = style.getPropertyValue('left');
+      // controls the opening and closing of the menu once hamburger menu is clicked
+      const menu = document.getElementById('menuId');
+      const style = window.getComputedStyle(menu);
+      const left = style.getPropertyValue('left');
 
       if (left == "0px"){
-         element.style.left = "-240px";
+         menu.style.left = "-240px";
       }
       else {
-         element.style.left = "0px";
+         menu.style.left = "0px";
+      }
+
+      // controls the width of the sidebar when the menu is open
+      const nav = document.getElementById('nav');
+      const navStyle = window.getComputedStyle(nav);
+      const navWidth = navStyle.getPropertyValue('width');
+
+      if (navWidth == '80px'){
+         nav.style.width = '240px';
+      }
+      else {
+         nav.style.width = '80px';
       }
    });
 
-
+      // controls animation on scroll
       AOS.init();
 });
